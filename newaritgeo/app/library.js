d8ee820e264912d5ff1e82d25cc06435368f4a06
Ogunbo Temilola <temmyogunbo@gmail.com>
2017-04-08 08:22:13 +0100

    It contains tests for arithmetric and geometric progresion

--- newaritgeo/app/library.js
+++ newaritgeo/app/library.js
@@ -17,7 +17,7 @@ aritGeo: function(array){
         len=array.length;
     for(i=1; i<len - 1; i++){   //loop starting from the second element in the array
 
-      var c=array[i +1] - array[i]       //difference of i+1 element and i
+      var c=array[i +1] - array[i];       //difference of i+1 element and i
 
         if (c === d){                   
             continue;      //if c===d , move to the next element in the array 
@@ -38,16 +38,16 @@ aritGeo: function(array){
               len = array.length;
           for (j=1; j<len - 1; j++){  //loop starting from the second element in the
          
-            var e = array[j + 1] /array[j]  //array difference of i+1 element and i
+            var e = array[j + 1] /array[j] ; //array difference of i+1 element and i
          
             if(e === r){  //the ratio of i+1 element to i
          
               continue;   //if c===d , move to the next element in the array 
          
             }
-            return  -1   //"neither Arithmetric or Geometric"
+            return  -1 ;  //"neither Arithmetric or Geometric"
           }
-          return "Geometric"
+          return "Geometric";
         }
 
 
